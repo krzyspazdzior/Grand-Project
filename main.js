b51027d2d485e48a63914b80db0052c5b08c211a
krzysKLP22 <1p22pazdzior@gmail.com>
2024-01-10 13:58:00 +0100

    footer + resposive layout

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-let options = document.querySelectorAll('.option');
-let selectedOption = localStorage.getItem('selectedOption');
+// let options = document.querySelectorAll('.option');
+// let selectedOption = localStorage.getItem('selectedOption');
 
-let i;
-function unselectAll() {
-    for (i = 0; i < options.length; i++) {
-        options[i].style.backgroundColor = "";
-    }
-}
-options.forEach(option => {
-   option.addEventListener('click', function(){
-      unselectAll();
-      this.style.backgroundColor = "#ff7e0e4c";
-      localStorage.setItem('selectedOption', this.innerText);
-   } );
-});
+// let i;
+// function unselectAll() {
+//     for (i = 0; i < options.length; i++) {
+//         options[i].style.backgroundColor = "";
+//     }
+// }
+// options.forEach(option => {
+//    option.addEventListener('click', function(){
+//       unselectAll();
+//       this.style.backgroundColor = "#ff7e0e4c";
+//       localStorage.setItem('selectedOption', this.innerText);
+//    } );
+// });
 
-        if (selectedOption) {
-            options.forEach(option => {
-                if (option.innerText === selectedOption) {
-                    option.style.backgroundColor = "#ff7e0e4c";
-                }
-            });
-        }
+//         if (selectedOption) {
+//             options.forEach(option => {
+//                 if (option.innerText === selectedOption) {
+//                     option.style.backgroundColor = "#ff7e0e4c";
+//                 }
+//             });
+//         }
