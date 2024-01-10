61f780df5e87a564dcde7fbdecdffb0395337c96
krzysKLP22 <1p22pazdzior@gmail.com>
2024-01-10 02:00:03 +0100

    navbar toggle not working

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let options = document.querySelectorAll('.option');
+let selectedOption = localStorage.getItem('selectedOption');
+
 let i;
 function unselectAll() {
     for (i = 0; i < options.length; i++) {
@@ -9,5 +11,14 @@ options.forEach(option => {
    option.addEventListener('click', function(){
       unselectAll();
       this.style.backgroundColor = "#ff7e0e4c";
+      localStorage.setItem('selectedOption', this.innerText);
    } );
 });
+
+        if (selectedOption) {
+            options.forEach(option => {
+                if (option.innerText === selectedOption) {
+                    option.style.backgroundColor = "#ff7e0e4c";
+                }
+            });
+        }
