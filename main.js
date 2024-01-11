aa77868b16faf594071500c5ce1cf17ee2c7775e
krzysKLP22 <1p22pazdzior@gmail.com>
2024-01-11 18:45:32 +0100

    hamburger fully working + download button

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 //Clicking menu shows options
-function show() {
-    document.getElementById('navbar-menu').classList.toggle('active');
-  }
+ function show() {
+   
+ }
 
 
 //hamburger menu --> X menu
 function hamburgerToX(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
+  document.querySelector('.mobile-menu').classList.toggle('active');
 }
